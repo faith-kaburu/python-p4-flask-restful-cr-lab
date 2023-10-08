2ffc9f0cda66bb0fa3436bb4fd7fb4d6e9d10ef9
faith kaburu <faith.kaburu@student.moringaschool.com>
2023-10-08 22:14:45 +0300

    initial commit

--- server/models.py
+++ server/models.py
@@ -7,3 +7,13 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+def __repr__(self):
+        return f'<Plant {self.name}, published at {self.created_at}.>'
+   
